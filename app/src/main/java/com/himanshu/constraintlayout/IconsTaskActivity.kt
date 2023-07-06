4fcc57872506136eca80fac9317263d668b7d2f5
Himanshu Padam <himanshupadam808@gmail.com>
2023-07-06 12:28:41 +0530

    Initial commit

---
+++ app/src/main/java/com/himanshu/constraintlayout/IconsTaskActivity.kt
@@ -0,0 +1,11 @@
+package com.himanshu.constraintlayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class IconsTaskActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_icons_task)
+    }
+}
